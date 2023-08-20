9d5941129e3a96863a7ecb84e564e0026b90c6e3
moxi <moxixiixi@gmail.com>
2023-08-20 18:41:24 +0800

    feat: add new post

--- src/components/Paper/index.tsx
+++ src/components/Paper/index.tsx
@@ -16,11 +16,11 @@ const Paper: FC<PaperProps> = ({
     <article
       className={`shadow-xl ${size} bg-paper m-5 border-default border-[1px] border-solid border-l-0 border-t-0 border-t-primary border-b-primary rounded-[4px] transition-all duration-700 ease`}
     >
-      <header className="text-primary w-full h-[80px] border-secondary border-l-[5px] border-solid flex flex-row justify-between items-center px-10">
+      <header className="text-primary w-full min-h-[80px] py-3 border-secondary border-l-[5px] border-solid flex flex-row justify-between items-center px-10">
         <h1 className="font-bold text-title">{title}</h1>
         <a href="/">
           <time
-            className="flex flex-row items-center gap-2 text-lg"
+            className="flex flex-row items-center gap-2 text-lg whitespace-nowrap"
             dateTime={dayjs(time).format("YYYY-MM-DD HH:mm:ss")}
           >
             <BsCalendarDate className="text-primary" />
